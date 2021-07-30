6da25c382d3a503e42a19e3049d892c09e41b76d
DESKTOP-MOON <gorbange@gmail.com>
2021-07-31 01:12:05 +0900

    게임매니저 추가

---
+++ gamejam_3team/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        { 
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

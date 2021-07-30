177fe07e703916053180201a57a8740a96053c45
DESKTOP-MOON <gorbange@gmail.com>
2021-07-31 01:34:00 +0900

    씬 수정, 씬 스크립트 추가, 폰트 추가

---
+++ gamejam_3team/Assets/02.Scripts/EndingScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum EndingType
+{
+    Bad,
+    Happy,
+}
+public class EndingScene : MonoBehaviour
+{
+    GameObject BadCut;
+    GameObject HappyCut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}

aa10853b8e8a895644f090efdea7c88af9403df0
DESKTOP-MOON <gorbange@gmail.com>
2021-07-31 03:18:10 +0900

    FadeInOut , TweenScale 코드 추가

---
+++ gamejam_3team/Assets/02.Scripts/FadeInOut.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FadeInOut : MonoBehaviour
+{
+    public MaskableGraphic target;
+    // Start is called before the first frame update
+    public Color curColor;
+    public Color targetColor;
+    public float duration = 1;
+    void Start()
+    {
+        StartCoroutine(Fade());
+    }
+
+    private IEnumerator Fade()
+    {
+        float time = 0f;
+        while(time<1)
+        {
+            target.color = Color.Lerp(curColor, targetColor, time / 1f);
+            time += Time.deltaTime/duration;
+            yield return null;
+
+        }
+        yield return null;
+
+    }
+}
